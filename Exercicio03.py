4141198e7cf5038abde892002f9e5b2163554fc6
MATHEUS SIMAO DA SILVA <00001093498213sp@aluno.educacao.sp.gov.br>
2024-05-22 17:36:34 -0300

    first commit

---
+++ Exercicio03.py
@@ -0,0 +1,5 @@
+quantidade_desejada = int(input("digite a quantidade desejada:"))
+if quantidade_desejada >=10:
+    print("produto disponivel em estoque. aproveite!")
+else:
+    print("produto indisponivel no momento. volte mais tarde!")
